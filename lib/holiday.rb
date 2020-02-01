f0c81b77036ec47089f3eac108e042ba2dbdd765
Daniel Sun <danielsun12345@gmail.com>
2020-02-01 04:25:08 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,7 +66,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
         puts "#{season.capitalize}:"
         holiday.each do|holidays, supplies|
-           puts "  #{holidays.to_s.delete("_").capitalize}: #{supplies.join", "}"
+           puts "  #{holidays.to_s.split("_").capitalize}: #{supplies.join", "}"
          end 
        end 
 end
