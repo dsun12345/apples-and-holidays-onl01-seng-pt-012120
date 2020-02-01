397fa4bd1d35701ce33c0a932c9bf0cc2a8ca157
Daniel Sun <danielsun12345@gmail.com>
2020-02-01 04:29:44 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,8 +74,8 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  holiday_hash.each do |season, holiday|
-    holiday.each do |holidays, supplies|
+  holiday_hash.map do |season, holiday|
+    holiday.map do |holidays, supplies|
       holidays if  item.include?("BBQ")
     end 
   end.flatten.compact
